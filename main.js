dd6334ce8f7a614c6037e065a1ad18a9d5444e8d
Diego Whitringtom Fabbri Ruguê <38573217+DiegoRugue@users.noreply.github.com>
2019-04-08 23:28:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 let cont = 0;
+let tabuleiro = [];
+
+tabuleiro['a'][1] = 0;
+tabuleiro['a'][2] = 0;
+tabuleiro['a'][3] = 0;
+
+tabuleiro['b'][1] = 0;
+tabuleiro['b'][2] = 0;
+tabuleiro['b'][3] = 0;
+
+tabuleiro['c'][1] = 0;
+tabuleiro['c'][2] = 0;
+tabuleiro['c'][3] = 0;
 
 function entrada(id) {
     let teste = document.getElementById(id);
@@ -6,9 +19,9 @@ function entrada(id) {
     cont ++;
 
     if (cont % 2 == 0) {
-        teste.innerHTML = '<img src="./img/X.jpg">'
+        teste.innerHTML = '<img src="./img/o.png">'
     } else {
-        teste.innerHTML = '<img src="./img/O.jpg">'
+        teste.innerHTML = '<img src="./img/x.png">'
     }
     
 }
