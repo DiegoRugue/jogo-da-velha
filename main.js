01ef064ca87874d2225587781d172d096b8773e8
Diego Whitringtom Fabbri Ruguê <38573217+DiegoRugue@users.noreply.github.com>
2019-04-10 16:28:57 -0300

    10/04

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let cont = 0;
+let jogoAtivo = true;
 let tabuleiro = Array(3);
 
 tabuleiro['a'] = Array(3);
@@ -21,23 +22,27 @@ function entrada(id) {
     let teste = document.getElementById(id);
 
     cont ++;
-
-    if (cont % 2 == 0) {
-        teste.innerHTML = '<img src="./img/x.png">';
-        let separa = id.split('_');
-        tabuleiro[separa[0]][separa[1]] = 'x';
-        if (verifica()) {
-            document.getElementById('resultado').innerHTML = verifica();
-        }
-    } else {
-        teste.innerHTML = '<img src="./img/o.png">';
-        let separa = id.split('_');
-        tabuleiro[separa[0]][separa[1]] = 'o';
-        if (verifica()) {
-            document.getElementById('resultado').innerHTML = verifica();
+    if (jogoAtivo) {
+        if (cont % 2 == 0) {
+            teste.innerHTML = '<img src="./img/x.png">';
+            document.getElementById(id).setAttribute('onclick', '');
+            let separa = id.split('_');
+            tabuleiro[separa[0]][separa[1]] = 'x';
+            if (verifica()) {
+                document.getElementById('resultado').innerHTML = verifica();
+                window.scrollTo(0,2000);
+            }
+        } else {
+            teste.innerHTML = '<img src="./img/o.png">';
+            document.getElementById(id).setAttribute('onclick', '');
+            let separa = id.split('_');
+            tabuleiro[separa[0]][separa[1]] = 'o';
+            if (verifica()) {
+                document.getElementById('resultado').innerHTML = verifica();
+                window.scrollTo(0,2000);
+            }
         }
     }
-
     console.log(tabuleiro);
 }
 
@@ -45,83 +50,100 @@ function verifica() {
     if(tabuleiro['a']['1'] == 'x' &&
        tabuleiro['a']['2'] == 'x' &&
        tabuleiro['a']['3'] == 'x') {
+           jogoAtivo = false;
            return "X é o vencedor";
 
     } else if (tabuleiro['a']['1'] == 'o' &&
                tabuleiro['a']['2'] == 'o' &&
                tabuleiro['a']['3'] == 'o') {
+                jogoAtivo = false;
                 return "O é o vencedor";
 
     } else if (tabuleiro['b']['1'] == 'x' &&
                tabuleiro['b']['2'] == 'x' &&
                tabuleiro['b']['3'] == 'x') {
+                jogoAtivo = false;
                 return "X é o vencedor";
 
     } else if(tabuleiro['b']['1'] == 'o' &&
               tabuleiro['b']['2'] == 'o' &&
               tabuleiro['b']['3'] == 'o') {
+                jogoAtivo = false;
                 return "O é o vencedor";
 
     } else if(tabuleiro['c']['1'] == 'x' &&
               tabuleiro['c']['2'] == 'x' &&
               tabuleiro['c']['3'] == 'x') {
+                jogoAtivo = false;
                 return "X é o vencedor";
 
     } else if(tabuleiro['c']['1'] == 'o' &&
               tabuleiro['c']['2'] == 'o' &&
               tabuleiro['c']['3'] == 'o') {
+                jogoAtivo = false;
                 return "O é o vencedor";
 
     } else if(tabuleiro['a']['1'] == 'x' &&
               tabuleiro['b']['1'] == 'x' &&
               tabuleiro['c']['1'] == 'x') {
+                jogoAtivo = false;
                 return "X é o vencedor";
 
     } else if(tabuleiro['a']['1'] == 'o' &&
               tabuleiro['b']['1'] == 'o' &&
               tabuleiro['c']['1'] == 'o') {
+                jogoAtivo = false;
                 return "O é o vencedor";
 
     } else if(tabuleiro['a']['2'] == 'x' &&
               tabuleiro['b']['2'] == 'x' &&
               tabuleiro['c']['2'] == 'x') {
+                jogoAtivo = false;
                 return "X é o vencedor";
 
     } else if(tabuleiro['a']['2'] == 'o' &&
               tabuleiro['b']['2'] == 'o' &&
               tabuleiro['c']['2'] == 'o') {
+                jogoAtivo = false;
                 return "O é o vencedor";
 
     } else if(tabuleiro['a']['3'] == 'x' &&
               tabuleiro['b']['3'] == 'x' &&
               tabuleiro['c']['3'] == 'x') {
+                jogoAtivo = false;
                 return "X é o vencedor";
 
     } else if(tabuleiro['a']['3'] == 'o' &&
               tabuleiro['b']['3'] == 'o' &&
               tabuleiro['c']['3'] == 'o') {
+                jogoAtivo = false;
                 return "O é o vencedor";
 
     } else if(tabuleiro['a']['1'] == 'x' &&
               tabuleiro['b']['2'] == 'x' &&
               tabuleiro['c']['3'] == 'x') {
+                jogoAtivo = false;
                 return "X é o vencedor";
 
     } else if(tabuleiro['a']['1'] == 'o' &&
               tabuleiro['b']['2'] == 'o' &&
               tabuleiro['c']['3'] == 'o') {
+                jogoAtivo = false;
                 return "O é o vencedor";
 
     } else if(tabuleiro['a']['3'] == 'x' &&
               tabuleiro['b']['2'] == 'x' &&
               tabuleiro['c']['1'] == 'x') {
+                jogoAtivo = false;
                 return "X é o vencedor";
 
     } else if(tabuleiro['a']['3'] == 'o' &&
               tabuleiro['b']['2'] == 'o' &&
               tabuleiro['c']['1'] == 'o') {
+                jogoAtivo = false;
                 return "O é o vencedor";
     } else if(cont == 9) {
-        return "Deu velha!"
+        jogoAtivo = false;
+        return `Deu velha! <img src='./img/palmirinha.jpg'>`
     }
 }
