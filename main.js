03f5e7916f5325003b2a999911d96d1d7e5b34e7
Diego Whitringtom Fabbri Ruguê <38573217+DiegoRugue@users.noreply.github.com>
2019-04-12 17:45:12 -0300

    Bot incompleto

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let cont = 1;
 let jogoAtivo = true;
-let boot = true;
+let boot = false;
 let c = 0;
+let para = false;
 let tabuleiro = Array(3);
 
 tabuleiro['a'] = Array(3);
@@ -19,6 +20,10 @@ tabuleiro['b']['3'] = 0;
 tabuleiro['c']['1'] = 0;
 tabuleiro['c']['2'] = 0;
 tabuleiro['c']['3'] = 0;
+
+function setBot() {
+    boot = true;
+}
 
 const inserirImagem = (img, id) => {
     //console.log(id);
@@ -153,31 +158,142 @@ function verifica() {
 }
 
 function inteligentao(id) {
+    para = false;
     let jogada = id.split('_');
     jogada = jogada.toString();
     console.log(jogada);
-    verificadeira('a');
-    c = 0;
-    verificadeira('b');
-    c = 0;
-    verificadeira('c');
-    c = 0;
+    // if (!para) {verificaDiagonal('o');}
+    // if (!para) {verificaDiagonalSecundaria('o');}
+    // if (!para) {verificaLinha('a', 'o');} 
+    // if (!para) {verificaLinha('b', 'o');}
+    // if (!para) {verificaLinha('c', 'o');}
+    // if (!para) {verificaDiagonalSecundaria('x');}
+    // if (!para) {verificaDiagonal('x');}
+    // if (!para) {verificaLinha('a', 'x');}
+    // if (!para) {verificaLinha('b', 'x');}
+    // if (!para) {verificaLinha('c', 'x');}
+    // if (!para) {verificaLinha('1', 'o');}
+    // if (!para) {verificaLinha('2', 'o');}
+    // if (!para) {verificaLinha('3', 'o');}
+    // if (!para) {verificaLinha('1', 'x');}
+    // if (!para) {verificaLinha('2', 'x');}
+    // if (!para) {verificaLinha('3', 'x');}
+    if (!para) {randown();}
 
 }
 
-const verificadeira = (indice) => {
+const verificaLinha = (indice, img) => {
     for (let i = 0; i < 3; i++) {
-        if (tabuleiro[indice][i] == 'x') {
+        if (tabuleiro[indice][i] == img) {
             c ++;
         } 
     }
+    console.log(c);
     if (c == 2) {
         if (tabuleiro[indice]['1'] == 0) {
-            inserirImagem("o", `${indice}_1`);
+            c = 0;
+            inserirImagem('o', `${indice}_1`);
+            return para = true;
         } else if (tabuleiro[indice]['2'] == 0) {
-            inserirImagem("o", `${indice}_2`);
+            c = 0;
+            inserirImagem('o', `${indice}_2`);
+            return para = true;
         } else if (tabuleiro[indice]['3'] == 0) {
-            inserirImagem("o", `${indice}_3`);
+            c = 0;
+            inserirImagem('o', `${indice}_3`);
+            return para = true;
         }
     }
 }
+
+const verificaColuna = (indice, img) => {
+    let col = ['a','b','c'];
+    let aux = 0;
+    for (let i = 0; i < 3; i++) {
+        if (tabuleiro[col[i]][indice] == img) {
+            c ++;
+        } 
+    }
+    console.log(c);
+    if (c == 2) {
+        for (let i = 0; i < 3; i++) {
+            if (tabuleiro[col[i]][indice] == 0) {
+                aux = i;
+            }
+        }
+        inserirImagem('o', indice[aux]);
+        return para = true;
+    }
+        
+}
+
+const verificaDiagonal = (img) => {
+    if (tabuleiro['a']['1'] == img) {
+        c ++;
+    }
+    if (tabuleiro['b']['2'] == img) {
+        c ++;
+    }
+    if (tabuleiro['c']['3'] == img) {
+        c ++;
+    }
+    console.log(c);
+    if (c == 2) {
+        if (tabuleiro['a']['1'] == 0) {
+            c = 0;
+            inserirImagem('o', 'a_1');
+            return para = true;
+        } else if (tabuleiro['b']['2'] == 0) {
+            c = 0;
+            inserirImagem('o', 'b_2');
+            return para = true;
+        } else if (tabuleiro['c']['3'] == 0) {
+            c = 0;
+            inserirImagem('o', 'c_3');
+            return para = true;
+        }
+    }
+}
+
+const verificaDiagonalSecundaria = (img) => {
+    if (tabuleiro['a']['3'] == img) {
+        c ++;
+    }
+    if (tabuleiro['b']['2'] == img) {
+        c ++;
+    }
+    if (tabuleiro['c']['1'] == img) {
+        c ++;
+    }
+    console.log(c);
+    if (c == 2) {
+        if (tabuleiro['a']['3'] == 0) {
+            c = 0;
+            inserirImagem('o', 'a_3');
+            return para = true;
+        } else if (tabuleiro['b']['2'] == 0) {
+            c = 0;
+            inserirImagem('o', 'b_2');
+            return para = true;
+        } else if (tabuleiro['c']['1'] == 0) {
+            c = 0;
+            inserirImagem('o', 'c_1');
+            return para = true;
+        }
+    }
+}
+
+const randown = () => {
+    let indice = ['a','b','c'];
+    let aleatorio1 = Math.floor(Math.random() * 3);
+    let aleatorio2 = Math.floor(Math.random() * 3);
+
+    if (tabuleiro[indice[aleatorio1]][aleatorio2] == 0) {
+        inserirImagem('o', `${indice[aleatorio1]}_${aleatorio2}`);
+        return para = true;
+    } else {
+        randown();
+    }
+}
+
+
